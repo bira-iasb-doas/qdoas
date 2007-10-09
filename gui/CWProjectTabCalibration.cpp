c64d69c123e2a9fe5e7272f1f10ba5d25648edf9
Ian Price <Ian Price@0551a244-8104-0410-bdc1-badfa85f4f5d>
2007-10-09 15:33:36 +0000

    removed obsolete header.
    git-svn-id: http://svn-ae/ae_doas/qdoas/trunk/Src@108 0551a244-8104-0410-bdc1-badfa85f4f5d

--- gui/CWProjectTabCalibration.cpp
+++ gui/CWProjectTabCalibration.cpp
@@ -27,7 +27,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "CWProjectTabCalibration.h"
 #include "CValidator.h"
-#include "CWCalibrationFitParametersDialog.h"
 
 #include "constants.h"
 
