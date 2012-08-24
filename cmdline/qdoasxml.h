78736f8aba3a1d4644f504a568402d3765479fb6
caroline <caroline@0551a244-8104-0410-bdc1-badfa85f4f5d>
2012-08-24 14:06:23 +0000

    Current QDOAS version including -xml switch in doas_cl
    git-svn-id: http://svn-ae/ae_doas/qdoas/trunk/Src@401 0551a244-8104-0410-bdc1-badfa85f4f5d

---
+++ cmdline/qdoasxml.h
@@ -0,0 +1,17 @@
+
+#include <cstdio>
+#include <cstring>
+
+#include <iostream>
+#include <string>
+
+#include <QString>
+#include <QList>
+#include <QStringList>
+
+#include "CProjectConfigItem.h"
+#include "CEngineResponse.h"
+#include "constants.h"
+#include "comdefs.h"
+
+RC QDOASXML_Parse(QList<QString> &xmlCommands,const CProjectConfigItem *p);
