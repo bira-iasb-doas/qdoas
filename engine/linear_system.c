c460bd3ee46f231205304ea18b52e1811dd1b008
thomasd <thomasd@0551a244-8104-0410-bdc1-badfa85f4f5d>
2017-03-06 10:05:15 +0000

    Add/update copyright and license statements.
    git-svn-id: http://svn-ae/ae_doas/qdoas/trunk/Src@1614 0551a244-8104-0410-bdc1-badfa85f4f5d

--- engine/linear_system.c
+++ engine/linear_system.c
@@ -1,3 +1,27 @@
+/* Copyright (C) 2017 Royal Belgian Institute for Space Aeronomy
+ * (BIRA-IASB)
+ *
+ * BIRA-IASB
+ * Ringlaan 3 Avenue Circulaire
+ * 1180 Uccle
+ * Belgium
+ * qdoas@aeronomie.be
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
