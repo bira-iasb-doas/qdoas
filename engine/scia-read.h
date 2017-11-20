7bea28806bc2e8571d362d20022a353f90721aa5
thomasd <thomasd@0551a244-8104-0410-bdc1-badfa85f4f5d>
2017-11-20 09:57:15 +0000

    Remove declaration for nonexistent function SCIA_GetRecordNumber().
    git-svn-id: http://svn-ae/ae_doas/qdoas/trunk/Src@1707 0551a244-8104-0410-bdc1-badfa85f4f5d

--- engine/scia-read.h
+++ engine/scia-read.h
@@ -4,7 +4,6 @@
 void SCIA_ReleaseBuffers(char format);
 RC   SCIA_SetPDS(ENGINE_CONTEXT *pEngineContext);
 RC   SCIA_ReadPDS(ENGINE_CONTEXT *pEngineContext,int recordNo);
-INDEX SCIA_GetRecordNumber(int hdfRecord,int obsNumber);
 void SCIA_get_orbit_date(int *year, int *month, int *day);
 RC SCIA_LoadAnalysis(ENGINE_CONTEXT *pEngineContext,void *responseHandle);
 RC SCIA_get_vza_ref(double esm_pos, int index_feno, FENO *feno);
