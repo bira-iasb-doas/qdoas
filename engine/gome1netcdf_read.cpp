ea8c58bd103d8dfda26b1265ca17f7d3604ee349
caroline <caroline@0551a244-8104-0410-bdc1-badfa85f4f5d>
2018-02-16 14:00:10 +0000

    Test another method to sort GOME pixels
    git-svn-id: http://svn-ae/ae_doas/qdoas/trunk/Src@1770 0551a244-8104-0410-bdc1-badfa85f4f5d

--- engine/gome1netcdf_read.cpp
+++ engine/gome1netcdf_read.cpp
@@ -190,7 +190,7 @@ struct clouddata
   vector<unsigned char> snow_ice_flag;                                          //!< \details Snow/ice flag (0 : normal)
   vector<unsigned char> sun_glint;                                              //!< \details Possible Sun-glint derived by a geometrical calculation using viewing angles (0 = no, 1 = yes)
  };
-} 
+}
 
 // ================
 // STATIC VARIABLES
@@ -531,6 +531,7 @@ RC GOME1NETCDF_Set(ENGINE_CONTEXT *pEngineContext)
   vector <double> deltatime;
   vector <double> deltatime_bs;
   vector <short> startpixel;
+  int *iscan,*iscan_bs,maxscan;
   int i,j,k,n;
   int currentScanIndex;
 
@@ -551,6 +552,8 @@ RC GOME1NETCDF_Set(ENGINE_CONTEXT *pEngineContext)
    channel_index=3;
   else
    channel_index=2;
+
+  iscan=iscan_bs=NULL;
 
   // Try to open the file and load metadata
 
@@ -607,8 +610,6 @@ RC GOME1NETCDF_Set(ENGINE_CONTEXT *pEngineContext)
         band_group.getVar("start_pixel",start,count,1,(short)0,startpixel);
        }
      }
-
-
 
     // Read backscans geodata and clouddata
 
@@ -668,80 +669,142 @@ RC GOME1NETCDF_Set(ENGINE_CONTEXT *pEngineContext)
 
     pEngineContext->n_alongtrack=0;
 
-    for (currentScanIndex=-1,                  // original scan line index
-         i=0,                                  // index to browse elements in scanline
-         j=0,                                  // index to browse elements in scanline_bs
-         k=0;
-       ((i<(int)scan_size) || (j<(int)scan_size_bs)) && (k<(int)pEngineContext->recordNumber) ;)   // !!!  && (pEngineContext->n_alongtrack<(int)scan_size)
+    maxscan=0;
+    if (scan_size && (scanline[(int)scan_size-1]>maxscan))
+     maxscan=scanline[(int)scan_size-1];
+    if (scan_size_bs && (scanline_bs[(int)scan_size_bs-1]>maxscan))
+     maxscan=scanline_bs[(int)scan_size_bs-1];
+    maxscan++;
+
+    if (((iscan=(int *)malloc(sizeof(int)*maxscan))==NULL) ||
+        ((iscan_bs=(int *)malloc(sizeof(int)*maxscan))==NULL))
+
+     rc=ERROR_ID_ALLOC;
+    else
      {
-      // Consider ground pixels
+      for (i=0;i<maxscan;i++)
+       iscan[i]=iscan_bs[i]=ITEM_NONE;
 
-      if ((i<(int)scan_size) && pixel_size )
+      if (scan_size)
+       for (i=0;i<(int)scan_size;i++)
+        iscan[scanline[i]]=i;
+
+      if (scan_size_bs)
+       for (i=0;i<(int)scan_size_bs;i++)
+        iscan_bs[scanline_bs[i]]=i;
+
+      for (i=k=0;i<maxscan;i++)
        {
-        for (n=0;n<(int)pixel_size;n++)
+        if ((j=iscan[i])!=ITEM_NONE)
          {
-          scanline_indexes[k+n]=i;
-          scanline_pixtype[k+n]=n;
-          scanline_pixnum[k+n]=scanline[i];
-          alongtrack_indexes[k+n]=pEngineContext->n_alongtrack;
-          delta_time[k+n]=delta_time_scan[i][n];
+          for (n=0;n<(int)pixel_size;n++)
+           {
+            scanline_indexes[k+n]=j;
+            scanline_pixtype[k+n]=n;
+            scanline_pixnum[k+n]=i;
+            alongtrack_indexes[k+n]=pEngineContext->n_alongtrack;
+            delta_time[k+n]=delta_time_scan[j][n];
+           }
+          k+=pixel_size;
          }
 
-        currentScanIndex=scanline[i];
-        k+=n;
-        i++;
-       }
-
-      // Consider back scan pixels (ground pixels could be missing : for example : 227 gb+bs, 228 bs, 229 gp -> avoid 227, 229, 228 bs and also infinite loops)
-
-      for (;(j<(int)scan_size_bs) && pixel_size_bs && (!pixel_size  || (i>=(int)scan_size) || (scanline_bs[j]<=currentScanIndex));)
-       {
-        // If only backscan pixel, sort is necessary
-
-       /* if (pixel_size && (scanline_bs[j]<currentScanIndex))
-         {
-          int k2;
-
-          for (k2=k-1;(k2>=0) && (scanline_pixnum[k2]>scanline_bs[j]);k2--)
-           {
-            scanline_indexes[k2+1]=scanline_indexes[k2];
-            scanline_pixtype[k2+1]=scanline_pixtype[k2];
-            scanline_pixnum[k2+1]=scanline_pixnum[k2];
-            alongtrack_indexes[k2+1]=alongtrack_indexes[k2]+1;
-            delta_time[k2+1]=delta_time[k2];
-           }
-
-           {
-            FILE *fp;
-            fp=fopen("toto.dat","a+t");
-            fprintf(fp,"--- %d %d %d %d %d %d \n",k,k2,i,j,alongtrack_indexes[k2+1],pEngineContext->n_alongtrack);
-            fclose(fp);
-           }
-
-          scanline_indexes[k2+1]=j;
-          scanline_pixtype[k2+1]=3;
-          scanline_pixnum[k2+1]=scanline_bs[j];
-          // alongtrack_indexes[k2+1]=pEngineContext->n_alongtrack;
-          delta_time[k2+1]=delta_time[k2]; // delta_time_scan_bs[j][0];
-         }
-
-        // Add backscan
-
-        else */
+        if ((j=iscan_bs[i])!=ITEM_NONE)
          {
           scanline_indexes[k]=j;
           scanline_pixtype[k]=3;
-          scanline_pixnum[k]=scanline_bs[j];
+          scanline_pixnum[k]=i;
           alongtrack_indexes[k]=pEngineContext->n_alongtrack;
           delta_time[k]=delta_time_scan_bs[j][0];
+          k++;
          }
 
-        k++;
-        j++;
+        if ((iscan[i]!=ITEM_NONE) || (iscan_bs[i]!=ITEM_NONE))
+         pEngineContext->n_alongtrack++;
        }
-
-      pEngineContext->n_alongtrack++;
      }
+
+    // Sort ground pixels and backscans using scanline
+
+    start_pixel=startpixel[0];
+
+    // scanline_indexes.resize(pEngineContext->recordNumber);
+    // scanline_pixtype.resize(pEngineContext->recordNumber);
+    // scanline_pixnum.resize(pEngineContext->recordNumber);
+    // alongtrack_indexes.resize(pEngineContext->recordNumber);
+    // delta_time.resize(pEngineContext->recordNumber);
+    //
+    // auto delta_time_scan=reinterpret_cast<const double(*)[pixel_size]>(deltatime.data());
+    // auto delta_time_scan_bs = reinterpret_cast<const double(*)[pixel_size_bs]>(deltatime_bs.data());
+    //
+    // pEngineContext->n_alongtrack=0;
+    //
+    // for (currentScanIndex=-1,                  // original scan line index
+    //      i=0,                                  // index to browse elements in scanline
+    //      j=0,                                  // index to browse elements in scanline_bs
+    //      k=0;
+    //    ((i<(int)scan_size) || (j<(int)scan_size_bs)) && (k<(int)pEngineContext->recordNumber) ;)   // !!!  && (pEngineContext->n_alongtrack<(int)scan_size)
+    //  {
+    //   // Consider ground pixels
+    //
+    //   if ((i<(int)scan_size) && pixel_size )
+    //    {
+    //     for (n=0;n<(int)pixel_size;n++)
+    //      {
+    //       scanline_indexes[k+n]=i;
+    //       scanline_pixtype[k+n]=n;
+    //       scanline_pixnum[k+n]=scanline[i];
+    //       alongtrack_indexes[k+n]=pEngineContext->n_alongtrack;
+    //       delta_time[k+n]=delta_time_scan[i][n];
+    //      }
+    //
+    //     currentScanIndex=scanline[i];
+    //     k+=n;
+    //     i++;
+    //    }
+    //
+    //   // Consider back scan pixels (ground pixels could be missing : for example : 227 gb+bs, 228 bs, 229 gp -> avoid 227, 229, 228 bs and also infinite loops)
+    //
+    //   for (;(j<(int)scan_size_bs) && pixel_size_bs && (!pixel_size  || (i>=(int)scan_size) || (scanline_bs[j]<=currentScanIndex));)
+    //    {
+    //     // If only backscan pixel, sort is necessary
+    //
+    //     if (pixel_size && (scanline_bs[j]<currentScanIndex))
+    //      {
+    //       int k2;
+    //
+    //       for (k2=k-1;(k2>=0) && (scanline_pixnum[k2]>scanline_bs[j]);k2--)
+    //        {
+    //         scanline_indexes[k2+1]=scanline_indexes[k2];
+    //         scanline_pixtype[k2+1]=scanline_pixtype[k2];
+    //         scanline_pixnum[k2+1]=scanline_pixnum[k2];
+    //         alongtrack_indexes[k2+1]=alongtrack_indexes[k2]+1;
+    //         delta_time[k2+1]=delta_time[k2];
+    //        }
+    //
+    //       scanline_indexes[k2+1]=j;
+    //       scanline_pixtype[k2+1]=3;
+    //       scanline_pixnum[k2+1]=scanline_bs[j];
+    //       // --> do not assign alongtrack here : alongtrack_indexes[k2+1]=pEngineContext->n_alongtrack;
+    //       delta_time[k2+1]=delta_time_scan_bs[j][0];
+    //      }
+    //
+    //     // Add backscan
+    //
+    //     else
+    //      {
+    //       scanline_indexes[k]=j;
+    //       scanline_pixtype[k]=3;
+    //       scanline_pixnum[k]=scanline_bs[j];
+    //       alongtrack_indexes[k]=pEngineContext->n_alongtrack;
+    //       delta_time[k]=delta_time_scan_bs[j][0];
+    //      }
+    //
+    //     k++;
+    //     j++;
+    //    }
+    //
+    //   pEngineContext->n_alongtrack++;
+    //  }
 
     GOME1NETCDF_Get_Irradiance(pEngineContext,channel_index,pEngineContext->buffers.lambda_irrad,pEngineContext->buffers.irrad);
 
@@ -756,6 +819,11 @@ RC GOME1NETCDF_Set(ENGINE_CONTEXT *pEngineContext)
    }
 
   // Return
+
+  if (iscan!=NULL)
+   free(iscan);
+  if (iscan_bs!=NULL)
+   free(iscan_bs);
 
   return rc;
 }
